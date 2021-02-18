cb5e7ee5521467adabaed3e888a07491f3ff56f3
Emmett Miller <emmett.miller93@gmail.com>
2021-02-17 22:12:20 -0800

    Add HTTPS option

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
 import express from 'express';
 
 import * as v1 from './v1';
@@ -18,15 +21,41 @@ if (!databaseUrl) {
     throw Error('The DATABASE_URL environment variable must be set.');
 }
 
-const port = +(process.env.PORT || 8080);
 const host = process.env.HOST || 'localhost';
+
+const httpEnabled = process.env.HTTP_ENABLED == 'true' || true;
+const httpPort = +(process.env.HTTP_PORT || 8080);
+
+const httpsEnabled = process.env.HTTPS_ENABLED == 'true' || false;
+const httpsPort = +(process.env.HTTPS_PORT || 8443);
+const privateKeyFile = process.env.PRIVATE_KEY_FILE;
+const certificateFile = process.env.CERTIFICATE_FILE;
+
+if (httpsEnabled && (!privateKeyFile || !certificateFile)) {
+    LOG.error("PRIVATE_KEY_FILE and CERTIFICATE_FILE environment variables are required for HTTPS.");
+    process.exit(-1);
+}
 
 LOG.info('App is starting up...');
 
 (async () => {
     const v1Api = await v1.init(databaseUrl);
 
-    express().use('v1', v1Api).listen(port, host);
+    const app = express().use('/v1', v1Api);
 
-    LOG.info('App is listening at {0}:{1}.', host, port);
+    if (httpEnabled) {
+        LOG.info("Initializing HTTP server at http://{0}:{1}.", host, httpPort);
+        http.createServer(app).listen(httpPort, host);
+    }
+
+    if (httpsEnabled) {
+        LOG.info("Initializing HTTPS server https://{0}:{1}.", host, httpsPort);
+        https.createServer(
+            {
+                key: fs.readFileSync(privateKeyFile),
+                cert: fs.readFileSync(certificateFile)
+            },
+            app
+        ).listen(httpsPort, host);
+    }
 })();
